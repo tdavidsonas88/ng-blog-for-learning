bb369c6aa0740fddc438ebee95eb38c5ee1d39c6
Tadas <tdavidsonas88@gmail.com>
2020-11-07 09:48:32 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-
+// Decorator
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'blog';
+  appName = 'blog app';
+  name = 'bruce';
+  getName(){
+    return this.name;
+  }
+  obj={
+    name: 'Kelly',
+    age: 20,
+  }
+  arr=['Bruce', 'Tonny', 'Peter'];
+  siteUrl = window.location.href;
+
 }
