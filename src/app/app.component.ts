11b9ad8a699b10e9c5d8dae32ba23621b013cb86
Tadas <tdavidsonas88@gmail.com>
2021-04-07 20:38:58 +0300

    Angular 10 tutorial #10 get input box value

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  title = 'blog';
+  currentVal = "";
+  getVal(val)
+  {
+    console.warn(val);
+    this.currentVal = val;
+  }
   appName = 'blog app';
   name = 'bruce';
   myName = 'Tadas';
